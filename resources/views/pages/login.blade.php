02da3d92cc9e2446e504221bcc016a40149edaa2
Christian Herdiyanto Prasetia <christianherdiyanto@gmail.com>
2023-12-13 12:28:30 +0700

    fix login and register pages

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -10,15 +10,16 @@
     <meta property="og:description" content="">
     <meta property="og:url" content="">
     <meta name="keywords" content="">
-    <link rel='icon' type="image/x-icon" href='{{ asset('assets/images/logo2.ico')}}'>
+    <link rel='icon' type="image/x-icon" href='{{ asset('assets/images/logo2.ico') }}'>
     <title>Edulink</title>
 
     <!--CSS-->
-    {{-- @vite('resources/css/app.css') --}}
+    @vite('resources/css/app.css')
 
-    
-    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" /> --}}
     <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
@@ -171,13 +172,68 @@
     </style>
 </head>
 
-<body class="bg-gradient-to-r from-[#0e1737] to-[#0e183b]">
+<body class="bg-[#8EACCD]" style="font-family: Poppins">
     <div id="loader" wire:loading
         class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gradient-to-r from-indigo-950 to-blue-950 flex flex-col items-center justify-center">
         <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
         <img src="{{ asset('assets/images/edulink2.png') }}" alt="Nefa Logo" class="w-24 xl:w-28">
     </div>
-    <div class="h-screen md:flex">
+    <div class="h-screen md:flex justify-center">
+        <div class="bg-[#F9F3CC] w-1/3 my-20 rounded-2xl shadow-xl">
+            <form role="form mx-5" method="POST" action="{{ route('login') }}" class="w-full max-w-lg  mx-auto my-20">
+                @csrf
+                <h2 class="ml-2">Welcome to</h2>
+                <h1 style="padding-top:1rem"
+                    class="ml-2 font-extrabold pb-3 font-['Poppins'] text-5xl bg-clip-text text-black">
+                    VirtuLib
+                </h1>
+                <h3 class="ml-2 my-2">Sign In</h3>
+                @if (session('success'))
+                    <div class="alert alert-success mb-3" style="color:white;font-weight:bold" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if (session('error'))
+                    <div class="alert alert-warning mb-3" style="color:white;font-weight:bold" role="alert">
+                        {{ session('error') }}
+                    </div>
+                @endif
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                            for="grid-first-name">
+                            Email </label>
+                        <input type="email" name="email"
+                            class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                            id="grid-first-name">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-black text-xs font-bold mb-2"
+                            for="grid-password">
+                            Password
+                        </label>
+                        <input type="password" name="password"
+                            class="appearance-none block w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                            id="grid-password">
+                    </div>
+                </div>
+                <div class="text-center mx-3 items-center">
+                    <button type="submit"
+                        class="shadow bg-[#8EACCD] w-full my-4 mx-auto hover:bg-[#6d90b7] duration-500 hover:text-white focus:shadow-outline focus:outline-none text-black font-bold py-4 rounded-lg">
+                        Sign In
+                    </button>
+                </div>
+                <div class="mx-3 text-black text-center">
+                    <div>Don’t you have account yet?</div>
+                    <div><a href="/register" class="text-blue-700 hover:text-blue-400"> Register for free</a></div>   
+                </div>
+            </form>
+        </div>
+    </div>
+
+    {{-- <div class="h-screen md:flex">
         <div class="relative overflow-hidden shadow-2xl md:flex w-1/2 bg-gradient-to-r from-fuchsia-600 to-violet-800 i justify-around items-center hidden"
             style="text-align:-webkit-center">
             <div>
@@ -185,7 +241,7 @@
                     <img src="{{ asset('assets/images/edulink2.png') }}" alt="Nefa Logo"
                         class="w-72 items-center xl:w-72">
                 </a>
-                <p class="text-white text-xl text-center font-[Fredoka] mt-1">Rahasia Keberhasilan Para Ahli Industri
+                <p class="text-white text-xl text-center  mt-1">Rahasia Keberhasilan Para Ahli Industri
                 </p>
             </div>
             <div class="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8">
@@ -196,18 +252,18 @@
             <div class="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
         </div>
         <div class="flex md:w-1/2 justify-center py-4 items-center">
-            <form role="form mx-5" method="POST" action="{{ route('login') }}" class="w-full max-w-lg font-[Fredoka]">
+            <form role="form mx-5" method="POST" action="{{ route('login') }}" class="w-full max-w-lg ">
                 @csrf
                 <h1 style="padding-top:3rem"
                     class="font-extrabold text-transparent text-center pb-10 font-['Fredoka'] text-5xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                     Login ke EduLink
                 </h1>
-                @if(session('success'))
+                @if (session('success'))
                 <div class="alert alert-success mb-5" style="color:white;font-weight:bold" role="alert">
                     {{ session('success') }}
                 </div>
                 @endif
-                @if(session('error'))
+                @if (session('error'))
                 <div class="alert alert-warning mb-5" style="color:white;font-weight:bold" role="alert">
                     {{ session('error') }}
                 </div>
@@ -244,128 +300,128 @@
                 </div>
             </form>
         </div>
-    </div>
+    </div> --}}
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-const movingImage = document.getElementById('movingImage');
-const container = document.getElementById('app');
+            const movingImage = document.getElementById('movingImage');
+            const container = document.getElementById('app');
 
-document.addEventListener('mousemove', function(event) {
-    const containerRect = container.getBoundingClientRect();
-    const maxX = containerRect.width - movingImage.clientWidth;
-    const maxY = containerRect.height - movingImage.clientHeight;
+            document.addEventListener('mousemove', function(event) {
+                const containerRect = container.getBoundingClientRect();
+                const maxX = containerRect.width - movingImage.clientWidth;
+                const maxY = containerRect.height - movingImage.clientHeight;
 
-    // Calculate the percentage of mouse position within the container
-    const percentX = (event.clientX - containerRect.left) / containerRect.width;
-    const percentY = (event.clientY - containerRect.top) / containerRect.height;
+                // Calculate the percentage of mouse position within the container
+                const percentX = (event.clientX - containerRect.left) / containerRect.width;
+                const percentY = (event.clientY - containerRect.top) / containerRect.height;
 
-    // Calculate new coordinates within the container boundaries
-    const newX = Math.min(Math.max(percentX * maxX, 0), maxX);
-    const newY = Math.min(Math.max(percentY * maxY, 0), maxY);
+                // Calculate new coordinates within the container boundaries
+                const newX = Math.min(Math.max(percentX * maxX, 0), maxX);
+                const newY = Math.min(Math.max(percentY * maxY, 0), maxY);
 
-    // Update the image position based on mouse coordinates
-    movingImage.style.transform = `translate(${newX}px, ${newY}px)`;
-});
-});
+                // Update the image position based on mouse coordinates
+                movingImage.style.transform = `translate(${newX}px, ${newY}px)`;
+            });
+        });
 
 
-    AOS.init();
-    // Burger menus
-    document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
+        AOS.init();
+        // Burger menus
+        document.addEventListener('DOMContentLoaded', function() {
+            // open
+            const burger = document.querySelectorAll('.navbar-burger');
+            const menu = document.querySelectorAll('.navbar-menu');
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
+            if (burger.length && menu.length) {
+                for (var i = 0; i < burger.length; i++) {
+                    burger[i].addEventListener('click', function() {
+                        for (var j = 0; j < menu.length; j++) {
+                            menu[j].classList.toggle('hidden');
+                        }
+                    });
+                }
+            }
+
+            // close
+            const close = document.querySelectorAll('.navbar-close');
+            const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+            if (close.length) {
+                for (var i = 0; i < close.length; i++) {
+                    close[i].addEventListener('click', function() {
+                        for (var j = 0; j < menu.length; j++) {
+                            menu[j].classList.toggle('hidden');
+                        }
+                    });
+                }
+            }
+
+            if (backdrop.length) {
+                for (var i = 0; i < backdrop.length; i++) {
+                    backdrop[i].addEventListener('click', function() {
+                        for (var j = 0; j < menu.length; j++) {
+                            menu[j].classList.toggle('hidden');
+                        }
+                    });
+                }
+            }
+        });
+        $(document).ready(function() {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 0) {
+                    $('#navbar').addClass('scrolled');
+                } else {
+                    $('#navbar').removeClass('scrolled');
                 }
             });
-        }
-    }
-
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
-
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-    });
-    $(document).ready(function() {
-    $(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-    $('#navbar').addClass('scrolled');
-    } else {
-     $('#navbar').removeClass('scrolled');
-    }
-    });
-    });
+        });
     </script>
 
     <script>
         const filterButtons = document.querySelectorAll('.filter-btn');
-    const portfolioItems = document.querySelectorAll('.portfolio-item');
+        const portfolioItems = document.querySelectorAll('.portfolio-item');
 
-    filterButtons.forEach(button => {
-    button.addEventListener('click', () => {
-  const filterValue = button.dataset.filter;
+        filterButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const filterValue = button.dataset.filter;
 
-  portfolioItems.forEach(item => {
-    if (filterValue === 'all' || item.classList.contains(filterValue)) {
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-    });
-});
-window.addEventListener('load', function () {
-const loader = document.getElementById('loader');
-const content = document.getElementById('content');
+                portfolioItems.forEach(item => {
+                    if (filterValue === 'all' || item.classList.contains(filterValue)) {
+                        item.style.display = 'block';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                });
+            });
+        });
+        window.addEventListener('load', function() {
+            const loader = document.getElementById('loader');
+            const content = document.getElementById('content');
 
-loader.style.display = 'none';
-content.style.display = 'block';
-});
-
-
+            loader.style.display = 'none';
+            content.style.display = 'block';
+        });
     </script>
     <script>
         var toTopButton = document.getElementById("to-top-button");
-    window.onscroll = function () {
-        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-            toTopButton.classList.remove("hidden");
-        } else {
-            toTopButton.classList.add("hidden");
+        window.onscroll = function() {
+            if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+                toTopButton.classList.remove("hidden");
+            } else {
+                toTopButton.classList.add("hidden");
+            }
         }
-    }
-    function goToTop() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
 
-
+        function goToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
     </script>
 </body>
 
